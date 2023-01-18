bc7daa4ae76750a72c3f5f8ffd5226cf6ef3795c
EC2 Default User <ec2-user@ip-172-31-44-194.ap-northeast-1.compute.internal>
2023-01-18 17:15:59 +0000

    1/19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #初期状態のdeviseは、サインアップ・サインイン時に「email」と「パスワード」しか受け取ることを許可されていない
+  #「name」も受け取れるように記述
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
