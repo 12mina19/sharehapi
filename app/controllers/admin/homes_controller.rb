f1ac1fe1c4f0b9889b0b2af1ed4dce228a197c6e
12mina19 <mgoh257@gmail.com>
2023-02-01 18:22:51 +0000

    2/2

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,12 +1,16 @@
 class Admin::HomesController < ApplicationController
 
-  def top
-
-    # if params[:category_id]
-    #   post_ids = PostCategory.where(category_id: params[:category_id]).pluck('post_id')
-    #   # post_ids => [1,4]
-    #   @posts = @posts.where(id: post_ids)
-    # end
+   def top
+   @posts = Post.where(params[:user_id]).order(created_at: :desc)
+   ##特定のユーザーに絞るのではなく、全ユーザーの投稿が表示されるようにparams[:user_id]だけを
+    # @posts = Post.where(user_id: params[:user_id]).order(created_at: :desc)
+  
+  
+  #   # if params[:category_id]
+  #   #   post_ids = PostCategory.where(category_id: params[:category_id]).pluck('post_id')
+  #   #   # post_ids => [1,4]
+  #   #   @posts = @posts.where(id: post_ids)
+  #   # end
 
     case params[:sort]
     when 'good'
@@ -15,13 +19,15 @@ class Admin::HomesController < ApplicationController
       @posts = @posts.order(created_at: :asc)
     else
       @posts = @posts.order(created_at: :desc)
+      # いいね順。ここあるとエラーでる。
     end
+    
 
     @categories = Category.all
     @comments = Comment.all
   end
 
-   protected
+  protected
 
   def post_params
     params.permit(:user_id, :category_id, :post_text, :image)
