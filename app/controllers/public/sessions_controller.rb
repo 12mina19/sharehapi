ee5b9c84263ecd09cc6e7465d44c49167dbbfa5f
12mina19 <mgoh257@gmail.com>
2023-02-09 11:23:45 +0000

    2/9

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -72,7 +72,8 @@ class Public::SessionsController < Devise::SessionsController
     if @user.valid_password?(params[:user][:password]) && @user.is_unpermitted == true
     flash[:notice] = "現在アカウントは利用停止中となっております。"
     redirect_to new_user_registration_path  #public/registrations#new 新規投稿画面へ
-    #ここでエラー文でるようにしてあげる。
+
+
     end
   end
 end
